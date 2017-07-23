6ae8dc7e4fd9da135d20c6860f2413818574ba1e
Martin Karlsen <1martinkarlsen@gmail.com>
2017-07-23 19:15:04 +0200

    Removed dropdownmodule from main navigation component

--- src/app/main/shared/navigation/navigation.component.ts
+++ src/app/main/shared/navigation/navigation.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DropdownModule } from 'ng2-dropdown';
 import { AuthenticationService } from './../../../services/authentication.service';
 
 @Component({
