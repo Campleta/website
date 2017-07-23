f44cbc7a4773012a8935ae47ce2f2ece17793301
Martin Karlsen <1martinkarlsen@gmail.com>
2017-07-23 18:47:06 +0200

    Fixed navbar dropdown

--- src/app/main/shared/navigation/navigation.component.ts
+++ src/app/main/shared/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DropdownModule } from 'ng2-dropdown';
 import { AuthenticationService } from './../../../services/authentication.service';
 
 @Component({
@@ -8,9 +9,16 @@ import { AuthenticationService } from './../../../services/authentication.servic
 })
 export class NavigationComponent implements OnInit {
 
+  isIn = false; // responsive collapse
+  public profileCollapse:boolean = false;
+
   constructor(public authService: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  toggleState() {
+    this.isIn = !this.isIn;
   }
 
 }
