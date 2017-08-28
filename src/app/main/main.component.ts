092b598a6ae7e745979d632fd1c4bd62ebf441ce
Martin Karlsen <martin@vixo.dk>
2017-08-28 13:58:20 +0200

    Revert "Get area types"

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from './../services/authentication.service';
 
 @Component({
   selector: 'app-main',
@@ -8,7 +7,7 @@ import { AuthenticationService } from './../services/authentication.service';
 })
 export class MainComponent implements OnInit {
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
   }
